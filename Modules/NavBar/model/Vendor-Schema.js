d1c68adce96fb8951f6666882596afae0d091dff
Harshit <harshitbhardwaj001@gmail.com>
2024-10-20 14:30:26 +0530

    Fixed Schemas and Services

--- Modules/NavBar/model/Vendor-Schema.js
+++ Modules/NavBar/model/Vendor-Schema.js
@@ -1,9 +1,8 @@
 import mongoose, { SchemaTypes } from "mongoose";
-export const VendorSchema = mongoose.Schema(
-    {
-        CategoryName:{type:SchemaTypes.String, required: true, minLength:3},
-        Description:{type:SchemaTypes.String, required: true, minLength:10},
-        Status:{type:SchemaTypes.String, required: true, minLength:5},
-    }
-)
-export const VendorSch=mongoose.model("Vendor-Category",VendorSchema);
+export const VendorSchema = mongoose.Schema({
+  Title: { type: SchemaTypes.String, required: true },
+  CategoryName: { type: SchemaTypes.String, required: true, minLength: 3 },
+  Description: { type: SchemaTypes.String, required: true, minLength: 10 },
+  Status: { type: SchemaTypes.String, required: true, minLength: 5 },
+});
+export const VendorSch = mongoose.model("Vendor-Category", VendorSchema);
--- Modules/User-Management/model/User-Schema.js
+++ Modules/User-Management/model/User-Schema.js
@@ -1,19 +1,26 @@
 import mongoose, { SchemaTypes } from "mongoose";
 
 export const UserSchema = mongoose.Schema({
-    Name: { type: SchemaTypes.String, required: true, minLength: 5 },
-    Mail: { type: SchemaTypes.String, required: true, minLength: 8, unique: true },
-    Password: { type: SchemaTypes.String, required: true, minLength: 8 },
-    ConfirmPassword: { type: SchemaTypes.String, required: true, minLength: 8 },
-    MobileNumber: { type: SchemaTypes.String, required: true }, // Changed to String for better validation
-    Role: { type: SchemaTypes.String, default: 'User' },
-    DOB: { type: SchemaTypes.Date, required: true },
-    LinkedinProfile: { type: SchemaTypes.String }, // Optional field
-    CompanyName: { type: SchemaTypes.String, required: true, minLength: 10 },
-    ProfessionalTitle: { type: SchemaTypes.String, required: true, minLength: 10 },
-    Status: { type: SchemaTypes.String, default: 'Active' }, // Default value
-    UserType: { type: SchemaTypes.String, default: 'Regular User', required: true, minLength: 3 },
-    UserImage: { type: SchemaTypes.String } // Optional field for storing image URL
+  Name: { type: SchemaTypes.String, required: true, minLength: 5 },
+  Mail: {
+    type: SchemaTypes.String,
+    required: true,
+    minLength: 8,
+    unique: true,
+  },
+  Password: { type: SchemaTypes.String, required: true, minLength: 8 },
+  ConfirmPassword: { type: SchemaTypes.String, required: true, minLength: 8 },
+  MobileNumber: { type: SchemaTypes.String, required: true }, // Changed to String for better validation
+  Role: { type: SchemaTypes.String, default: "User" },
+  DOB: { type: SchemaTypes.Date, required: true },
+  LinkedinProfile: { type: SchemaTypes.String }, // Optional field
+  CompanyName: { type: SchemaTypes.String, required: true, minLength: 3 },
+  ProfessionalTitle: { type: SchemaTypes.String, required: true },
+  Status: { type: SchemaTypes.String, default: "Inactive" }, // Default value
+  UserType: { type: SchemaTypes.String, default: "Regular User", minLength: 3 },
+  UserImage: { type: SchemaTypes.String }, // Optional field for storing image URL
+  Currency: { type: SchemaTypes.String, default: "Dollar" },
+  Language: { type: SchemaTypes.String, default: "English" },
 });
 
 export const userSch = mongoose.model("user-management", UserSchema);
