c80c394258f8c9b1bbefc9d68967f0ea19f9eeab
Vishaltakshak <127527299+Vishaltakshak@users.noreply.github.com>
2025-01-13 18:58:09 +0530

    1.84

--- App.js
+++ App.js
@@ -15,9 +15,16 @@ import morgan from "morgan";
 dotenv.config({ path: "./env" }); 
 
 const corsOptions = {
-  origin: ["http://localhost:3500", process.env.ADMIN_URL, process.env.HOSTED_URL],
+  origin: ["http://localhost:3500","https://ceo-backend-six.vercel.app", process.env.ADMIN_URL, process.env.HOSTED_URL],
   methods: ["GET", "POST", "PUT", "DELETE"], 
-  allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With'],
+  allowedHeaders: [
+    'Content-Type', 
+    'Authorization', 
+    'X-Requested-With',
+    'Access-Control-Allow-Origin',
+    'Origin',
+    'Accept'
+  ],
   credentials: true,
   maxAge: 86400 
 };
@@ -31,19 +38,6 @@ const port = process.env.PORT || 3500;
 App.use(express.json());
 
 // Define routes
-
-
-const startServer = async () => {
-  try {
-    await CreatConnection();
-    App.listen(port, () => {
-      console.log("Server is running on port", port);
-    });
-  } catch (err) {
-    console.error("Error while starting server:", err);
-    process.exit(1);
-  }
-};
 App.get('/', (req, res) => {
   res.send('Backend is working!');
 });
@@ -56,3 +50,18 @@ App.use("/api/NavBar", NavBarRoutes);
 App.use("/api/subnav/link", OnHoverLink);
 App.use("/api/user", UserRoutes);
 App.use("/api/Vendor", VendorRoutes);
+App.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Something broke!');
+});
+const startServer = async () => {
+  try {
+    await CreatConnection();
+    App.listen(port, () => {
+      console.log("Server is running on port", port);
+    });
+  } catch (err) {
+    console.error("Error while starting server:", err);
+    process.exit(1);
+  }
+};
